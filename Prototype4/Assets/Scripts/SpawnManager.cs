5b6fb9da5f8c4620d22424969b82a8aac1fce2c3
Nathaniel Doromal <nathanielrd98@gmail.com>
2022-06-08 17:04:57 -0700

    Update SpawnManager.cs

--- Prototype4/Assets/Scripts/SpawnManager.cs
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -6,15 +6,32 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab; // need reference to enemy prefabs
 
+    private float spawnRange = 9.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+
+        //Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+        //Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // this function will return a Vector3, randomPos
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
     }
 }
