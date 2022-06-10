232c22e52b28f293d8e1f4a6d5d206b8306ba483
Nathaniel Doromal <nathanielrd98@gmail.com>
2022-06-10 05:13:51 -0700

    Finalized p4 mod

--- Prototype4/Assets/Challenge 4/Scripts/EnemyX.cs
+++ Prototype4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -8,12 +8,16 @@ public class EnemyX : MonoBehaviour
     private Rigidbody enemyRb;
     private GameObject playerGoal;
 
+    public AudioClip scoreSound;
+    private AudioSource goalSoundSource;
+
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
         playerGoal = GameObject.Find("Player Goal"); // reference the Player Goal object
         speed = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>().enemySpeed; //reference enemy speed in Spawn manager
+        goalSoundSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -30,7 +34,9 @@ public class EnemyX : MonoBehaviour
         // If enemy collides with either goal, destroy it
         if (other.gameObject.name == "Enemy Goal")
         {
+            goalSoundSource.PlayOneShot(scoreSound, 1.0f);
             Destroy(gameObject);
+            
         } 
         else if (other.gameObject.name == "Player Goal")
         {
